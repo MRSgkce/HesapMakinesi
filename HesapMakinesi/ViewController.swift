025d2d6e0cab0c53b2aadadc7dd166cb6a4479b3
MRSgkce <mursidegokce985@gmail.com>
2024-09-23 17:48:43 +0300

    toplama işlemi yapıldı AC ile sıfırlandı fakat iki label kullanıldı.

--- HesapMakinesi/ViewController.swift
+++ HesapMakinesi/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var hesap:[String] = []
 
+    @IBOutlet weak var islem: UILabel!
+    @IBOutlet weak var sonuc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func esittir(_ sender: Any) {
+        
+        // Önce hesap dizisindeki tüm öğeleri birleştiriyoruz
+            let hesapString = hesap.joined(separator: "+") // "1+2+3" gibi bir string oluşturur
+            
+            // Sonra bu string'i split(separator:) ile ayırıyoruz
+            let full = hesapString.split(separator: "+") // [Substring] döner
+            
+            // Substring'leri String'e çevirip bir Array'e dönüştürelim.
+            let fullArray = full.map { String($0) }
+            
+            print(fullArray) // ["1", "2", "3"] gibi bir çıktı verecek
+            
+        // Stringleri Int'e çevirip toplamı hesaplayalım
+            let toplam = fullArray.compactMap { Int($0) }.reduce(0, +)
+            
+        print(toplam)
+        sonuc.text = "\(toplam)"
+        
 
+    }
+    
+    @IBAction func topla(_ sender: Any) {
+        if let veri = islem.text{
+            hesap.append(veri)
+            sonuc.text = hesap.joined(separator: "+")
+        }
+    }
+    @IBAction func sifirla(_ sender: Any) {
+        sonuc.text = ""
+        hesap.removeAll()
+    }
+    @IBAction func bir(_ sender: Any) {
+        islem.text = "1"
+    }
+    
+    @IBAction func iki(_ sender: Any) {
+        islem.text = "2"
+    }
+    @IBAction func üc(_ sender: Any) {
+        islem.text = "3"
+    }
+    @IBAction func dort(_ sender: Any) {
+        
+    }
+    @IBAction func bes(_ sender: Any) {
+    }
+    @IBAction func alti(_ sender: Any) {
+    }
+    @IBAction func yedi(_ sender: Any) {
+    }
+    @IBAction func sekiz(_ sender: Any) {
+    }
+    @IBAction func dokuz(_ sender: Any) {
+    }
 }
 
